c2b00dc620e8621e3edffc621eb564fffb1ebb62
HinDayNi <gthithanhhien04@gmail.com>
2026-05-19 21:42:33 +0700

    feat: implement ChatPage component

--- src/pages/user/ChatPage.tsx
+++ src/pages/user/ChatPage.tsx
@@ -490,7 +490,6 @@ export default function ChatPage() {
     onOpenCreateGroup: openCreateGroupModal,
     onOpenMarkRead: handleOpenMarkRead,
     onOpenAddFriend: handleOpenAddFriend,
-    onGroupClick: handleGroupClick,
     onToggleConversationMute: convPrefs.handleToggleConversationMute,
   };
 
